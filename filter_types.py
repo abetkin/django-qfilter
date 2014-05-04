5810da8da6ba366ae8d813267e35af7588609210
Vitalii Abetkin <abetkin.vv@vuaro.ru>
2014-05-04 21:44:58 +0400

    filter container in progress

--- filter_types.py
+++ filter_types.py
@@ -34,6 +34,23 @@ class QuerysetIterationHook(QuerySetFilter):
     
     def __init__(self, hook_function):
         self.hook_function = hook_function
+
+    def _combine(self, other):
+        #TODO: add support for lazy operations
+        assert isinstance(other, QuerysetIterationHook)
+        hook_function = CallablesList.from_callables(
+                [self.hook_function, other.hook_function], None)
+        return self.__class__(hook_function)
+    
+    def __and__(self, other):
+        if isinstance(other, QuerysetIterationHook):
+            return self._combine(other)
+        return super(QuerysetIterationHook, self).__and__(other)
+    
+    def __or__(self, other):
+        if isinstance(other, QuerysetIterationHook):
+            return self._combine(other)
+        return super(QuerysetIterationHook, self).__or__(other)
     
     def __call__(self, queryset):
         class QuerysetWrapper(type(queryset)):
@@ -46,11 +63,36 @@ class QuerysetIterationHook(QuerySetFilter):
 
 #%%
 
+class LazyOperator(object):
+    '''
+    like `and`: f() and g() # g might not get executed
+    '''
+    #TODO
+
+#%%
+
 class CallablesList(list):
     
     def __init__(self, *args, **kw):
         self.operation = kw.pop('operation', lambda x,y: None)
         super(CallablesList, self).__init__(*args, **kw)
+    
+    @classmethod
+    def from_callables(cls, callables, operation=None):
+        '''
+        Assuming some of callables may be instances of `CallablesList`
+        construct a new instance with a list of plain functions.
+        '''
+        def flat_list():
+            for callabl in callables:
+                try:
+                    for f in callabl:
+                        yield f
+                except TypeError:
+                    yield callabl
+        operation = operation or (lambda x,y: None)
+        return cls(flat_list(), operation=operation)
+    
     
     def __call__(self, *args, **kw):
         results = [f(*args, **kw) for f in self]
@@ -76,28 +118,22 @@ class ValuesDictFilter(QuerySetFilter):
         if fields_list:
             self.fields_list = list(fields_list)
     
-    def _apply(self, other, operation):
+    def _apply_op(self, operation, other):
         assert isinstance(other, ValuesDictFilter)
-        def list_callables():
-            for callabl in (self.filter_func, other.filter_func):
-                try:
-                    for f in callabl:
-                        yield f
-                except TypeError:
-                    yield callabl
-        filter_func = CallablesList(list_callables(), operation=operation)
-        return self.__class__(filter_func=filter_func,
-                              fields_list=self.fields_list + other.fields_list)
+        fields_list=self.fields_list + other.fields_list
+        filter_func = CallablesList.from_callables(
+                [self.filter_func, other.filter_func], operation)
+        return self.__class__(filter_func, fields_list)
     
     def __and__(self, other):
         if isinstance(other, ValuesDictFilter):
-            return self._apply(other, operation=lambda x, y: x and y)
+            return self._apply_op(lambda x, y: x and y, other)
         return super(ValuesDictFilter, self).__and__(other)
     
     def __or__(self, other):
         if isinstance(other, ValuesDictFilter):
-            return self._apply(other, operation=lambda x, y: x or y)
-        return super(ValuesDictFilter, self).__or__(other)
+            return self._apply_op(lambda x, y: x or y, other)
+        return super(ValuesDictFilter, self).__and__(other)
     
     def __call__(self, queryset):
         fields_list = ['pk'] + self.fields_list
@@ -127,7 +163,7 @@ def fname(obj):
 #%%
 import operator
 from unicom.crm.models import BankCompany
-f = filtr | fname
+f = filtr & fname
 f
 
 #%%
@@ -141,8 +177,16 @@ qs.count()
 def set_hvost(obj):
     obj.hvost = 30
 
-qs = set_hvost(BankCompany.objects.filter(id=11978))
-qs[0].hvost
+@QuerysetIterationHook
+def set_color(obj):
+    obj.color = 'grey'
+
+f = set_color | set_hvost
+
+#%%
+
+qs = f(BankCompany.objects.filter(id=11978))
+qs[0].color + ' ' + str(qs[0].hvost)
 #%%
 
 #%%
--- filters.py
+++ filters.py
@@ -15,17 +15,15 @@ class self_is__dict__(dict):
         self.__dict__ = self
 
 
-class FilterSet(object):
+class FiltersContainer(object):
 
-    _method_name = None
-    
-    filter_type = None # filter type is taken into account when combining filters
-    context = None
+    _context = {}
     
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
         self.context = self.get_context()
     
+    ## small trick for convenience
     @property
     def context(self):
         return self._context
@@ -35,52 +33,60 @@ class FilterSet(object):
         assert isinstance(dic, dict), 'context should be a dict'
         self._context = self_is__dict__(dic)
     
+    ##
+    
+    #TODO: make instance for each filter
+    
     def get_context(self):
         'for overriding'
         return self.context
-    
-    def __iter__(self):
-        'depends on grouping setting'
 
-    def as_filter(self):
+    '''
+    Assume we use method markers, smth. like
+    ...
+    '''
+    
+    def _add_queryset_arg(self, method):
         def callabl(queryset):
-            method = getattr(self, self._method_name)
             self.context.queryset = queryset
             return method()
-        klass = self._dispatch(self._method_name)
-        return klass(callabl)
+        return callabl
 
-    @classmethod
-    def _dispatch(cls, method_name):
-        return 'filter class'
+    def make_filter(self, callabl):
+        '''
+        `method` is an instance method made to accept queryset argument
+        '''
+        marker_args = method.marker_args
+        marker_kwargs = method.marker_kwargs
+        
+        if marker_args and marker_args[0] == 'filter_function':
+            return lambda self: None
+        return filter_types.QFilter
+        
+    def make_callable(self, method):
+        1
+
+
+#    @classmethod
+#    def get_filter_class(cls, method_marker):
+#        '''Method markers are strings in the simplest case.'''
+#        if method_marker == 'filter function':
+#            return filter_types.ValuesDictFilter
+#        if method_marker == 'iteration hook':
+#            return filter_types.QuerysetIterationHook
+#        return filter_types.QFilter
 
     @classmethod
     def is_filter_method(cls, name):
         return name.startswith('filter__')
 
     @classmethod
-    def _find_methods(cls):
-        'return {method_name: method_info}'
-        def gen():
-            for name in dir(cls):
-                method = getattr(cls, name)
-                if not inspect.ismethod(method) or not cls.is_filter_method(name):
-                    continue
-                info = cls._collect_method_info(name)
-                yield name, info
-        return dict(gen())
+    def _get_methods_names(cls):
+        attrs = dir(cls)
+        def filter_func(name):
+            method = getattr(cls, name)
+            return inspect.ismethod(method) and cls.is_filter_method(name)
+        return filter(filter_func, attrs)
 
-    @classmethod
-    def _collect_method_info(cls, method_name):
-        method = getattr(cls, method_name)
-        if hasattr(method, 'type'):
-            return {'class': cls.get_filter_class(method.type)}
-        return {'class': filter_types.SimpleQuerysetFilter}
-    
-    filter_types = {'queryset': filter_types.SimpleQuerysetFilter}
-    
-    @classmethod
-    def get_filter_class(cls, filter_type):
-        return cls.filter_types.get(filter_type) 
 #
 
